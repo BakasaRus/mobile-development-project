50383bef718b79c6ea1587a8c4158134d0ddb7f5
Nikolay Andreev <bakasaru@list.ru>
2021-10-13 19:57:42 +0300

    Add listView click event handler

--- app/src/main/java/ru/itmo/fitp/mobile/MainActivity.kt
+++ app/src/main/java/ru/itmo/fitp/mobile/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.itmo.fitp.mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,5 +19,12 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.names))
         val listView = findViewById<ListView>(R.id.names_list)
         listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+//            val element = adapter.getItemAtPosition(position) // The item that was clicked
+//            val intent = Intent(this, BookDetailActivity::class.java)
+            val intent = Intent(this, DetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
