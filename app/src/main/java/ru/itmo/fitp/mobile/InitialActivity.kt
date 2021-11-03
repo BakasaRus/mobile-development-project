d281cb1b427d632463fb61c8267c43c2bacab026
Nikolay Andreev <bakasaru@list.ru>
2021-11-03 12:38:39 +0300

    Add points counter in InitialActivity

--- app/src/main/java/ru/itmo/fitp/mobile/InitialActivity.kt
+++ app/src/main/java/ru/itmo/fitp/mobile/InitialActivity.kt
@@ -1,16 +1,45 @@
 package ru.itmo.fitp.mobile
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.forEach
 
 class InitialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial)
+
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        var points = sharedPreferences.getInt("points", 0)
+        val pointsField = findViewById<TextView>(R.id.pointsField)
+        pointsField.text = points.toString()
+
+        val plusButton = findViewById<Button>(R.id.plusButton)
+        val minusButton = findViewById<Button>(R.id.minusButton)
+
+        plusButton.setOnClickListener {
+            points++
+            pointsField.text = points.toString()
+            sharedPreferences.edit().putInt("points", points).apply()
+            plusButton.isEnabled = points < 100
+            minusButton.isEnabled = points > 0
+        }
+        minusButton.setOnClickListener {
+            points--
+            pointsField.text = points.toString()
+            sharedPreferences.edit().putInt("points", points).apply()
+            plusButton.isEnabled = points < 100
+            minusButton.isEnabled = points > 0
+        }
+
         val button = findViewById<Button>(R.id.toast_button)
         button.setOnClickListener {
             Toast.makeText(applicationContext, "Test", Toast.LENGTH_SHORT).show()
