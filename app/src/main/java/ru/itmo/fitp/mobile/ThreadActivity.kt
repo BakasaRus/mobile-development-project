9218b7524717adb739a37f283f166b49df68c259
Nikolay Andreev <bakasaru@list.ru>
2021-12-15 14:31:51 +0300

    Add simple threads for counters

--- app/src/main/java/ru/itmo/fitp/mobile/ThreadActivity.kt
+++ app/src/main/java/ru/itmo/fitp/mobile/ThreadActivity.kt
@@ -2,10 +2,37 @@ package ru.itmo.fitp.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.concurrent.thread
 
 class ThreadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thread)
+
+        val slowCounterLabel = findViewById<TextView>(R.id.slowIncLabel)
+        val fastCounterLabel = findViewById<TextView>(R.id.fastIncLabel)
+
+        val slowCounter = thread(name = "SlowCounter") {
+            var counter = 0
+            while (true) {
+                counter++
+                runOnUiThread {
+                    slowCounterLabel.text = counter.toString()
+                }
+                Thread.sleep(600)
+            }
+        }
+
+        val fastCounter = thread(name = "FastCounter") {
+            var counter = 0
+            while (true) {
+                counter++
+                runOnUiThread {
+                    fastCounterLabel.text = counter.toString()
+                }
+                Thread.sleep(400)
+            }
+        }
     }
 }
